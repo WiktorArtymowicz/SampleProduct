050ecfc23be6a424fa792294aa551aaf5908eccb
Wiktor Artymowicz <kurt44pl@onet.eu>
2020-05-26 23:01:45 +0200

    Add var SampleFiltered for LINQ and Supplier class

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace SampleProduct
         static void Main(string[] args)
         {
             List<Product> products = Product.GetSampleList();
+            List<Supplier> suppliers = Supplier.GetSuppliers();
             products.Sort(new ProductNameComparer());
 
             foreach (var item in products)
@@ -42,6 +43,18 @@ namespace SampleProduct
             foreach (var item in products.Where(p => p.Price > 35))
             {
                 Console.WriteLine(item.ToString());
+            }
+
+            Console.WriteLine();
+
+            var sampleFiltered = from p in products
+                                 join s in suppliers
+                                 on p.Id equals s.Id
+                                 select new { ProductName = p.Name, SupplierName = s.Name };
+
+            foreach (var item in sampleFiltered)
+            {
+                Console.WriteLine($"Produkt: {item.ProductName} Dostwaca: {item.SupplierName}");
             }
         }
     }
---
+++ Supplier.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleProduct
+{
+    public class Supplier
+    {
+        private int? _id = null;
+        private string _name = string.Empty;
+
+        public Supplier(int? id, string name)
+        {
+            _id = id;
+            _name = name;
+        }
+
+        public int? Id { get => _id; }
+        public string Name { get => _name; }
+
+        public static List<Supplier> GetSuppliers()
+        {
+            return new List<Supplier>()
+            {
+                new Supplier(id: 1, name: "Dostwca1"),
+                new Supplier(id: 2, name: "Dostawca2")
+            };
+        }
+    }
+}
